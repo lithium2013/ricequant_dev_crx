ad1fd8cc156439d28fffc633de0196942bfcbbd7
Lithium <lithium@ricequant.com>
2017-07-06 22:08:51 +0800

    [UPDATE]rm useless code

--- assets/ext/inject.js
+++ assets/ext/inject.js
@@ -1,3 +1,14 @@
-window.addEventListener('error', e => {
-    alert(`Uncaught js error : ${e.message}`)
-});
+//window.addEventListener('error', e => {
+//    alert(`Uncaught js error : ${e.message}`)
+//});
+
+const xhr = new XMLHttpRequest();
+
+xhr.onreadystatechange = () => {
+	if (xhr.readyState === 4 && xhr.status === 200) {
+		console.log(xhr.response, 'gg');
+	}
+};
+
+xhr.open('GET', 'https://locolhost:10888', true);
+xhr.send(null);
